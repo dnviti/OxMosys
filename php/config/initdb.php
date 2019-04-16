0c153401aa790e3ad7469b9162062566b29dbca5
Daniele Viti <dnviti@gmail.com>
2019-04-16 08:27:29 +0200

    Update initdb.php

--- php/config/initdb.php
+++ php/config/initdb.php
@@ -345,14 +345,14 @@ class InitDB
 
         // Inserisco le causali personalizzate
         if ($this->tablesStatus["app_warehouse_causals"]) {
-            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `code`, `descri`, `userreg`, `userupdate`) VALUES (1, 'CARICO_FORNITORE', 'Carico per acquisto merce presso fornitore', 1, 1)");
-            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `code`, `descri`, `userreg`, `userupdate`) VALUES (2, 'SCARICO_NEGOZIO', 'Scarico per vendita al negozio', 1, 1)");
-            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `code`, `descri`, `userreg`, `userupdate`) VALUES (3, 'SCARICO_MATTEW', 'Scarico per vendita su facebook', 1, 1)");
-            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `code`, `descri`, `userreg`, `userupdate`) VALUES (4, 'SCARICO_ECOMMERCE', 'Scarico per vendita su e-commerce', 1, 1)");
-            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `code`, `descri`, `userreg`, `userupdate`) VALUES (5, 'SCARICO_RESO_FORNITORE', 'Scarico da reso fornitore', 1, 1)");
-            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `code`, `descri`, `userreg`, `userupdate`) VALUES (6, 'CARICO_RESO_CLIENTE', 'Carico da reso cliente', 1, 1)");
-            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `code`, `descri`, `userreg`, `userupdate`) VALUES (7, 'CARICO_RETTIFICA_INVENTARIALE', 'Carico rettifica inventario', 1, 1)");
-            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `code`, `descri`, `userreg`, `userupdate`) VALUES (8, 'SCARICO_RETTIFICA_INVENTARIALE', 'Scarico rettifica inventario', 1, 1)");
+            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `type`, `code`, `descri`, `userreg`, `userupdate`) VALUES (1, '+', 'CARICO_FORNITORE', 'Carico per acquisto merce presso fornitore', 1, 1)");
+            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `type`, `code`, `descri`, `userreg`, `userupdate`) VALUES (2, '-', 'SCARICO_NEGOZIO', 'Scarico per vendita al negozio', 1, 1)");
+            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `type`, `code`, `descri`, `userreg`, `userupdate`) VALUES (3, '-', 'SCARICO_MATTEW', 'Scarico per vendita su facebook', 1, 1)");
+            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `type`, `code`, `descri`, `userreg`, `userupdate`) VALUES (4, '-', 'SCARICO_ECOMMERCE', 'Scarico per vendita su e-commerce', 1, 1)");
+            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `type`, `code`, `descri`, `userreg`, `userupdate`) VALUES (5, '-', 'SCARICO_RESO_FORNITORE', 'Scarico da reso fornitore', 1, 1)");
+            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `type`, `code`, `descri`, `userreg`, `userupdate`) VALUES (6, '+', 'CARICO_RESO_CLIENTE', 'Carico da reso cliente', 1, 1)");
+            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `type`, `code`, `descri`, `userreg`, `userupdate`) VALUES (7, '+', 'CARICO_RETTIFICA_INVENTARIALE', 'Carico rettifica inventario', 1, 1)");
+            self::$appDbConn->query("INSERT INTO `app_warehouse_causals` (`id`, `type`, `code`, `descri`, `userreg`, `userupdate`) VALUES (8, '-', 'SCARICO_RETTIFICA_INVENTARIALE', 'Scarico rettifica inventario', 1, 1)");
         }
     }
 
